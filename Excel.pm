ac32dd0d05ec0c6f222547186c12dc8b31ea15e7
Ilya Sterin <isterin@hotmail.com>
2001-05-15 17:25:08 +0000

    Import of ISTERIN/XML-Excel-0.02 from CPAN.
    gitpan-cpan-distribution: XML-Excel
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ISTERIN/XML-Excel-0.02.tar.gz
    gitpan-cpan-author:       ISTERIN
    gitpan-cpan-maturity:     released

--- Excel.pm
+++ Excel.pm
@@ -100,9 +100,7 @@ sub parse_doc
         	for(my $cell_count = $worksheet->{MinCol}; defined $worksheet->{MaxCol} && $cell_count <= $worksheet->{MaxCol} ; $cell_count++) 
         	{
             		$cell = $worksheet->{Cells}[$row_count][$cell_count];
-            		next unless defined($cell);
-            		push(@cols_data, $cell->Value);
-            		
+            		defined($cell) ? push(@cols_data, $cell->Value) : push(@cols_data, undef);            		
         	}
         	$escape_char->(\@cols_data);
         	push(@arr_cols_data, \@cols_data);
@@ -124,6 +122,8 @@ sub print_xml
 	$args->{parent_tag} = "record" unless $args->{parent_tag}; 
 	$args->{format} = "\t" unless $args->{format};  #default {format} to tab if not supplied
 	
+	$class->{'document_element'} = $args->{file_tag};  ### Used later for declare_doctype() method
+	
 	if ($class->{'column_data'} == 0 || ($class->{'column_headings'} == 0 && $class->{'headings'}))
 	{
 		croak "There is no data to print, make sure that you parsed the document before printing";
@@ -137,6 +137,12 @@ sub print_xml
 		*FILE_OUT = *STDOUT;
 	}
 	
+	print FILE_OUT $class->{'declare_xml'}."\n" if $class->{'declare_xml'};
+	###This will replace the non-interpolated $class->{'document_element'} inside the $class->{'declare_doctype'} to get the real value
+	###Should be replace with something more practical in the future...
+	$class->{'declare_doctype'} =~ s/\$class\-\>\{\'document_element\'\}/$class->{'document_element'}/ if $class->{'declare_doctype'};
+	
+	print FILE_OUT $class->{'declare_doctype'}."\n" if $class->{'declare_doctype'};
 	print FILE_OUT "<$args->{file_tag}>", "\n";	### print initial document tag
 		
 	### declare the $tag for <$tag> and $loop_num for headers and data index tracking
@@ -156,14 +162,14 @@ sub print_xml
 			print FILE_OUT $args->{format}, "</$args->{parent_tag}>", "\n";
 		}
 	
-	} else {  ### if column headings are not provided we default to $tag
+	} else {  ### if column headings are not provided we default to <tr$loop_num>
 		
 		foreach $loop_num (0..$#{$class->{'column_data'}})
 		{
 	       		print FILE_OUT $args->{format}, "<$args->{parent_tag}>", "\n";
 			foreach $tag (0..$#{$class->{'column_data'}->[$loop_num]})
 			{
-				print FILE_OUT $args->{format}, $args->{format}, "<$tag>$class->{'column_data'}[$loop_num][$tag]</$tag>\n";
+				print FILE_OUT $args->{format}, $args->{format}, "<tr$loop_num>$class->{'column_data'}[$loop_num][$tag]</tr$loop_num>\n";
 			}
 			print FILE_OUT $args->{format}, "</$args->{parent_tag}>", "\n";
 		}
@@ -175,6 +181,81 @@ sub print_xml
 	
 }
 
+sub declare_xml
+{
+
+	my $class = shift;
+	my $attr = shift || {};
+	
+	### Attributes: version, encoding, standalone
+
+	if (exists $attr->{'version'})
+	{
+		$class->{'declare_xml'} = "<?xml version=\"$attr->{'version'}\"" 
+	}
+	else
+	{
+		$csvxml_error = "The version attribute must be specified for declare_xml()\n
+				Usage: declare_xml\({version=>1.0, [encoding=>..., standalone=>yes/no]}\)";
+		croak "$csvxml_error" if ($class->{'error_out'} == 1);		
+	}
+	
+	$class->{'declare_xml'} .= " encoding=\"$attr->{'encoding'}\"" if exists $attr->{'encoding'};
+	if (exists $attr->{'standalone'} && ($attr->{'standalone'} =~ /[yes|no]/))
+	{
+		$class->{'declare_xml'} .= " standalone=\"$attr->{'standalone'}\""; 
+	}
+	elsif (!($attr->{'standalone'} =~ /[yes|no]/))
+	{
+		$csvxml_error = "The standalone attribute must be yes|no for declare_xml()\n
+				Usage: declare_xml\({version=>1.0, [encoding=>..., standalone=>yes/no]}\)";
+		croak "$csvxml_error" if ($class->{'error_out'} == 1);
+	}
+	
+	$class->{'declare_xml'} .= "?>";
+		
+	return $class->{'declare_xml'};
+
+}
+
+sub declare_doctype
+{
+	
+	my $class = shift;
+	my $attr = shift || {};
+	
+	### Attributes: source, location1, location2, subset
+
+	$class->{'declare_doctype'} = '<!DOCTYPE $class->{\'document_element\'}';
+	if ($attr->{source} eq "SYSTEM" || $attr->{source} eq "PUBLIC")
+	{
+		$class->{'declare_doctype'} .= " $attr->{'source'}";
+	}
+	else
+	{
+		$csvxml_error = "The source attribute is not set correctly";
+		croak "$csvxml_error" if ($class->{'error_out'} == 1);
+	}
+	
+	if (exists $attr->{location1} && !(exists $attr->{subset}))
+	{
+		$class->{'declare_doctype'} .= " \"$attr->{'location1'}\"";
+	}
+	else
+	{
+		$csvxml_error = "$attr->{'source'} location1 must be specified";
+		croak "$csvxml_error" if ($class->{'error_out'} == 1);
+	}
+	
+	$class->{'declare_doctype'} .= " \"$attr->{'location2'}\"" if exists $attr->{'location2'};
+	$class->{'declare_doctype'} .= " [$attr->{'subset'}]" if exists $attr->{'subset'};
+	
+	
+	$class->{'declare_doctype'} .= ">";
+	
+	return $class->{'declare_doctype'};
+	
+}
 
 $get_header = sub()
 {
@@ -189,7 +270,7 @@ $get_header = sub()
         	for(my $cell_count = $worksheet->{MinCol}; defined $worksheet->{MaxCol} && $cell_count <= $worksheet->{MaxCol} ; $cell_count++) 
         	{
             		$cell = $worksheet->{Cells}[$row_count][$cell_count];
-            		push(@$ref_col, $cell->Value);
+            		defined($cell) ? push(@$ref_col, $cell->Value) : push(@$ref_col, undef);            		
         	}
     	}
 	
@@ -219,6 +300,7 @@ $escape_char = sub()  ### Escape char per XML 1.0 specifications
 			@{$arg}[$arr_index] =~ s/\>/\&gt\;/g;
 			@{$arg}[$arr_index] =~ s/\'/\&apos\;/g;
 			@{$arg}[$arr_index] =~ s/\"/\&quot\;/g;
+			@{$arg}[$arr_index] =~ s/([\x80-\xFF])/$XmlUtf8Encode->(ord($1))/ge;
 		}
 	}
 	elsif (ref($arg) eq 'SCALAR')
@@ -227,13 +309,31 @@ $escape_char = sub()  ### Escape char per XML 1.0 specifications
 		${$arg} =~ s/\</\&lt\;/g;
 		${$arg} =~ s/\>/\&gt\;/g;
 		${$arg} =~ s/\'/\&apos\;/g;
-		${$arg} =~ s/\"/\&quot\;/g;		
+		${$arg} =~ s/\"/\&quot\;/g;	
+		${$arg} =~ s/([\x80-\xFF])/$XmlUtf8Encode->(ord($1))/ge;			
 	}
 	else
 	{
 		croak "Usage: $escape_char->(\@cols_data) or $escape_char->(\$foo)\n";
 	}
 		
+};
+
+$XmlUtf8Encode = sub() {
+
+    my $n = shift;
+    if ($n < 0x80) {
+        return chr ($n);
+    } elsif ($n < 0x800) {
+        return pack ("CC", (($n >> 6) | 0xc0), (($n & 0x3f) | 0x80));
+    } elsif ($n < 0x10000) {
+        return pack ("CCC", (($n >> 12) | 0xe0), ((($n >> 6) & 0x3f) | 0x80),
+                     (($n & 0x3f) | 0x80));
+    } elsif ($n < 0x110000) {
+        return pack ("CCCC", (($n >> 18) | 0xf0), ((($n >> 12) & 0x3f) | 0x80),
+                     ((($n >> 6) & 0x3f) | 0x80), (($n & 0x3f) | 0x80));
+    }
+    return $n;
 };
   
 
@@ -243,7 +343,7 @@ $escape_char = sub()  ### Escape char per XML 1.0 specifications
 
 1;
 __END__
-# Below is the stub of documentation for your module. You better edit it!
+
 
 =head1 NAME
 
@@ -257,6 +357,9 @@ XML::Excel - Perl extension converting Excel files to XML
 
   $status = $excel_obj->parse_doc(file_name);
   $status = $excel_obj->parse_doc(file_name, \%attr);
+  
+  $excel_obj->declare_xml(\%attr);
+  $excel_obj->declare_doctype(\%attr);
 
   $excel_obj->print_xml(file_name, \%attr);
 
@@ -276,6 +379,21 @@ implemented in a soon coming release.  As the module will provide both: object a
 be used upon individual needs, system resources, and required performance.  Ofcourse the DOM
 implementation takes up more resources and in some instances timing, it's the easiest to use.
 
+=head1 ATTRIBUTES new()
+
+error_out - Turn on the error handling which will die on all errors and assign the error message to
+$XML::Excel::_error.
+
+column_headings - Specifies the column heading to use.  Passed as an array reference.  Can be used 
+as a supplement to using the first column in the file as the XML tag names.  Since XML::Excel does 
+not require you to parse the Excel file, you can provide your own data structure to parse.
+
+column_data - Specifies the Excel data in a two dimensional array.  Passed as an array reference.
+
+ParseExcel - Specifies the ParseExcel object to use.  This is used to create custom ParseExcel object and override
+the default one created by XML::Excel.
+
+
 =head1 ATTRIBUTES parse_doc()
 
 headings - Specifies the number of rows to use as tag names.  Defaults to 0.
@@ -286,8 +404,39 @@ replaced with.  Defaults to undef meaning no substitution is done.  To eliminate
 characters use "" (empty string) or to replace with another see below.
 Ex.  {sub_char => "_"} or {sub_char => ""}
 
-ParseExcel = Allows to provide custom Spreadsheet::ParseExcel object to XML::Excel.           
-           
+
+=head1 ATTRIBUTES declare_xml()
+
+version - Specifies the xml version.  
+Ex.  {version => '1.0'}
+
+encoding - Specifies the type of encoding.  XML standard defaults encoding to 'UTF-8' if notspecifically
+           set.
+Ex.  {encoding => 'ISO-8859_1'}
+
+standalone - Specifies the the document as standalone (yes|no).  If the document is does not rely on an
+             external DTD, DTD is internal, or the external DTD does not effect the contents of the document,
+             the standalone attribute should be set to 'yes', otherwise 'no' should be used.  For more info
+             see XML declaration documentation.
+Ex.  {standalone => 'yes'}
+
+=head1 ATTRIBUTES declare_doctype()
+
+source - Specifies the source of the DTD (SYSTEM|PUBLIC)
+Ex. {source => 'SYSTEM'}
+
+location1 - URI to the DTD file.  Public ID may be used if source is PUBLIC.
+Ex. {location1 => 'http://www.xmlproj.com/dtd/index_dtd.dtd'} or {location1 => '-//Netscape Communications//DTD RSS 0.90//EN'}
+
+location2 - Optional second URI.  Usually used if the location1 public ID is not found by the
+            validating parser.
+Ex. {location2 => 'http://www.xmlproj.com/file.dtd'}
+
+subset - Any other information that proceedes the DTD declaration.  Usually includes internal DTD if any.
+Ex. {subset => 'ELEMENT first_name (#PCDATA)>\n<!ELEMENT last_name (#PCDATA)>'}
+You can even enterpolate the string with $obj->{column_headings} to dynamically build the DTD.
+Ex. {subset => "ELEMENT $obj->{columnt_headings}[0] (#PCDATA)>"}
+        
            
 =head1 ATTRIBUTES print_xml()
 
@@ -324,6 +473,10 @@ $excel_obj = XML::Excel->new();
 $excel_obj->{column_headings} = \@arr_of_headings;
 
 $excel_obj->parse_doc("in_file.xls");
+
+$excel_obj->declare_xml({version => '1.0', encoding => 'UTF-8', standalone => 'yes'});
+$excel_obj->declare_doctype({source => 'PUBLIC', location1 => '-//Netscape Communications//DTD RSS 0.90//EN', location2 => 'http://my.netscape.com/publish/formats/rss-0.91.dtd'});
+
 $excel_obj->print_xml("out.xml", {format => " ", file_tag = "xml_file", parent_tag => "record"});
 
 
